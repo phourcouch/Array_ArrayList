19181deaedd73d69e804243eb83333eee3f346c4
phourcouch <75496481+phourcouch@users.noreply.github.com>
2024-04-19 19:06:43 -0700

    Add files via upload

---
+++ src/arrayandarraylist/ArrayAndArrayLIst.java
@@ -0,0 +1,68 @@
+
+package arrayandarraylist;
+import java.util.ArrayList;
+
+public class ArrayAndArrayLIst {
+
+    public static void main(String[] args) {
+//          int list[] = {1,2,3,4,5};
+//        for (int i = 0; i < list.length;i++)
+//          System.out.println(list[i]);
+System.out.println((int)(Math.random()*2));
+        
+        System.out.println((int)(Math.random()*2));
+        
+//        ArrayList<Integer> arrayList = new ArrayList<Integer>();
+//        Integer ptr7 = new Integer (7);
+//        arrayList.add(ptr7);
+//        arrayList.add(new Integer(-1));
+//        arrayList.add(new Integer(25));
+//        Integer ptr25 = new Integer (25);
+//        arrayList.add(ptr25);
+//        arrayList.add(new Integer(4));
+//        
+//        System.out.println("==========="); 
+//        for (int i = 0; i<arrayList.size(); i++)
+//             System.out.println(arrayList.get(i).intValue()); 
+//        
+//        arrayList.remove(2);
+//         System.out.println("===========");
+//      for (int value : list)
+//           System.out.println(value);
+//      
+//       arrayList.add(new Integer(15));
+//         System.out.println("===========");
+//      for (int value : list)
+//           System.out.println(value);
+//      
+//       System.out.println("===========");
+//      for (Integer intVal : arrayList)
+//           System.out.println(intVal.intValue());
+//      
+//      
+//       System.out.println("===========");
+//      if(arrayList.contains(ptr7))
+//             System.out.println("true");
+//      else 
+//           System.out.println("false");
+//      
+//      System.out.println("===========");
+//      if(arrayList.contains(ptr25))
+//             System.out.println("true");
+//      else 
+//           System.out.println("false");
+//      
+//      arrayList.clear();
+//      
+//      System.out.println(arrayList.size());
+//      System.out.println("===========");
+//      for (int value : list)
+//           System.out.println(value);
+// 
+      
+    }
+    
+    
+    
+    
+}
